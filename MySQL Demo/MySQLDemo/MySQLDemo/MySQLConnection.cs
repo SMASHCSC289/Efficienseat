26bd6fd34667a8575fb8eec3e8691c931562538b
mharriett <disneyfreknc@yahoo.com>
2016-02-06 09:54:22 -0500

    Adding MySQL Demo Code

---
+++ MySQL Demo/MySQLDemo/MySQLDemo/MySQLConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace MySQLDemo
+{
+    public class MySQLConnection
+    {
+        string connectionstring = "server=localhost;uid=CAPSTONE;pwd=CAPSTONE;database=CAPSTONE;";
+        MySqlConnection DatabaseConnection;
+
+        public MySQLConnection()
+        {
+            DatabaseConnection = new MySqlConnection(connectionstring);
+        }
+
+        public MySqlConnection Connection
+        {
+            get
+            {
+                return DatabaseConnection;
+            }
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return connectionstring;
+            }
+        }
+
+
+        public void EstablishConnection()
+        {
+            try
+            {
+                DatabaseConnection.Close();
+                DatabaseConnection.ConnectionString = connectionstring;
+                DatabaseConnection.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void CloseConnection()
+        {
+            DatabaseConnection.Close();
+        }
+    }
+}
